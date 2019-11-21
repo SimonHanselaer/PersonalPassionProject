4f0369fdd928d3f028696b76de3f6738c51ff065
Simon Hanselaer <simon.hanselaer@student.howest.be>
2019-11-21 17:27:00 +0100

    commit before adding apollo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const electron = require('electron')
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
 
 let url
 if (process.env.NODE_ENV === 'DEV') {
@@ -8,6 +8,29 @@ if (process.env.NODE_ENV === 'DEV') {
 } else {
     url = `file://${process.cwd()}/dist/index.html`
 }
+
+const psList = require("ps-list");
+
+(async () => {
+    console.log(await psList()) => [{ pid: 3213, name: 'node', cmd: 'node test.js', ppid: 1, uid: 501, cpu: 0.1, memory: 1.5 }, …]
+})();
+
+// const desktopCapturer = require('electron');
+
+// desktopCapturer.getSources({ types: ['window', 'screen'] }).then(async sources => {
+//     console.log(sources);
+// });
+
+// desktopCapturer.getSources(
+//     {
+//         types: ["window", "screen"]
+//     },
+//     function (error, sources) {
+//         for (let source of sources) {
+//             console.log("Name: " + source.name);
+//         }
+//     }
+// );
 
 app.on('ready', () => {
     let window = new BrowserWindow({ width: 800, height: 600 })
